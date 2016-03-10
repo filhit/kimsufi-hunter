812cf88ce6abe42d4019161b0282e8bd7de60a22
filhit <filhit@gmail.com>
2016-03-10 21:43:08 +0100

    initial commit

---
+++ kimsufi-hunter.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name           kimsufi-hunter
+// @description    Hunts for available servers at kimsufi.com
+// @include        https://www.kimsufi.com/fr/serveurs.xml*
+// @version        1.0
+// ==/UserScript==
+
+(function() {
+    window.onload = function () {
+        if (isServerAvailable()) {
+            notifyUser();
+        }
+        else {
+            waitAndReloadPage();
+        }
+    }
+    
+    function isServerAvailable() {
+        return document.querySelector("[data-ref=\"160sk1\"] .elapsed-time-since-last-delivery[style*=\"display: none\"]");
+    }
+    
+    function notifyUser() {
+        alert("buy!");
+    }
+    
+    function waitAndReloadPage() {
+        setInterval(function () { location.reload();}, 10000);
+    }
+})();
